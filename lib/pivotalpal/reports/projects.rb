3afadeaf39e0863686d9969c5535c87d01e191af
Joshua Simmons <joshua@castironcoding.com>
2015-04-20 08:58:50 -0700

    [FEATURE DELIVERS #92076768] Adds backlog reports for single projects and for all projects

--- lib/pivotalpal/reports/projects.rb
+++ lib/pivotalpal/reports/projects.rb
@@ -13,21 +13,31 @@ module Pivotalpal
       def all
         table(:border => true) do
           row do
-            column('PROJECT', :width => 40)
-            column('OWNER', :width => 10)
-            column('BACKLOG', :width => 8)
-            column('ICEBOX', :width => 8)
-            column('ITER. LENGTH', :width => 8)
+            column('PROJECT', :width => 40, :color => 'white')
+            column('OWNER', :width => 10, :color => 'white')
+            column('BACKLOG', :width => 8, :color => 'white')
+            column('ICEBOX', :width => 8, :color => 'white')
+            column('ITER. LENGTH', :width => 8, :color => 'white')
           end
           client = Pivotalpal::Factory::PTClient::get
           projects = client.projects.sort_by {|v| v.name}
           projects.each do |project|
             owners = project.memberships.select { |m| m.role == 'owner' }.map { |m| m.person.initials }.join(', ')
+            backlogs = project.stories(:filter => 'state:unstarted')
+            iceboxes = project.stories(:filter => 'state:unscheduled')
             row do
-              column(project.name)
-              column(owners)
-              column(project.stories(:filter => 'state:unstarted').length)
-              column(project.stories(:filter => 'state:unscheduled').length)
+              column(project.name, :color => 'red')
+              column(owners, :color => 'yellow')
+              if backlogs.length == 0 # Easy to weed out 0s from substantial tallies
+                column(backlogs.length, :color => 'white')
+              else
+                column(backlogs.length, :color => 'cyan')
+              end
+              if iceboxes.length == 0
+                column(iceboxes.length, :color => 'white')
+              else
+                column(iceboxes.length, :color => 'red')
+              end
               column(project.iteration_length)
             end
           end
@@ -35,56 +45,71 @@ module Pivotalpal
 
       end
 
-      def single_backlog(proj)
+      def get_project(proj)
         client = Pivotalpal::Factory::PTClient::get
-        projects = client.projects.sort_by{|v| v.name}
+        projects = client.projects.sort_by {|v| v.name}
         projects.each do |project|
           if project.name.downcase == proj.downcase
-            story_repository = Pivotalpal::Repository::StoryRepository.new(:project => project)
-            header(:title => project.name,
-                   :align => 'center',
-                   :color => 'red',
-                   :rule => true,
-                   :bold => true,
-                   :width => 40)
+            return project
+          end
+        end
+        false
+      end
 
-            table(:border => true) do
-              row do
-                column('MEMBER', :width => 10,
-                                 :color => 'blue')
-                column('# of BACKLOGS', :width => 15,
-                                        :color => 'blue')
-              end
+      def backlog_single(proj)
+        if get_project(proj)
+          @project_query = get_project(proj)
+        else
+          header(:title => "That projjy ain't found! (Run 'list_projects' to see proper project titles)", :color => 'red')
+        end
 
-              members = project.memberships.select { |m| m.role == 'member' || m.role == 'owner' }.map { |m| m.person.initials }.join(',')
-              members.split(",").each do |member|
+        if defined? @project_query
+          backlog_total = 0
+          header(:title => @project_query.name,
+                 :align => 'center',
+                 :color => 'red',
+                 :bold => true,
+                 :rule => true,
+                 :width => 40)
+
+          table(:border => true) do
+            row do
+              column('MEMBER', :width => 10, :color => 'white')
+              column('# of BACKLOGS', :width => 15, :color => 'white')
+            end
+
+            members = @project_query.memberships.select { |m| m.role == 'member' || m.role == 'owner' }.map { |m| m.person.initials }.join(',')
+            members.split(",").each do |member|
+              backlog_count = @project_query.stories(:filter => "state:unstarted owner:#{member.upcase}").length
+              if backlog_count == 0
                 row do
-                  column(member.upcase, :color => 'red')
-                  column(story_repository.get_by_filters(['state:unstarted', "owner:#{member.upcase}"]).length, :color => 'green')
+                  column(member.upcase, :color => 'yellow')
+                  column(backlog_count, :color => 'white')
+                end
+
+              else
+                row do
+                  column(member.upcase, :color => 'yellow')
+                  column(backlog_count, :color => 'cyan')
                 end
               end
+              backlog_total += backlog_count
+            end
+            row do
+              column('TOTAL', :color => 'red')
+              column(backlog_total, :color => 'red')
             end
           end
         end
+
     end
 
-    # def all_backlogs
-    #   client = Pivotalpal::Factory::PTClient::get
-    #   projects = client.projects.sort_by
-
-    def get_members
-      final_members = []
+    def backlog_all
       client = Pivotalpal::Factory::PTClient::get
-      projects = client.projects.sort_by{|v| v.name}
+      projects = client.projects.sort_by {|v| v.name}
       projects.each do |project|
-        members = project.memberships.select { |m| m.role == 'member' }.map { |m| m.person.initials }.join(',')
-        members.split(",").each do |member|
-          if not final_members.include? member
-            final_members.push(member)
-          end
-        end
+        backlog_single(project.name)
       end
-      puts final_members
     end
 
     def list_projects
